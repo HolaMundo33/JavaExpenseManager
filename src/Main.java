c72dd6c05e9e38e5c8faa4b9774933dee9a6d6f1
AntonioMonzo4 <amonansu@gmail.com>
2024-04-26 19:45:41 +0200

    Añadida estructura switch a falta de las funciones

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 // Main application class that provides a console interface for managing expenses.
+
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String args[]){
+
+
+        //Variables de código
+        int opcion;
+        Scanner sc = Scanner(System.in);
+
+
         System.out.println("Bienvenido al Sistema de Registro de Gastos Personales ");
 
         System.out.println("\nPor favor, elige una opción: ");
@@ -12,9 +23,42 @@ public class Main {
         System.out.println("5. Cargar gastos");
         System.out.println("6. Salir");
 
-        System.out.println("prueba");
+        System.out.println("Tu opción: ");
+        opcion=sc.nextInt();
+
+        switch (opcion) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+    
+                break;
+
+            
+        }
 
 
+    }
+
+    private static Scanner Scanner(InputStream in) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'Scanner'");
     }
 
     
