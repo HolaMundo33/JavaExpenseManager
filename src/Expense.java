b9c91a4f813077393c757d5061446dfda0d3aa64
AntonioMonzo4 <amonansu@gmail.com>
2024-04-26 19:58:11 +0200

    Creada nueva rama e inicio de constructor de la clase Expense

--- src/Expense.java
+++ src/Expense.java
@@ -1,4 +1,6 @@
 // This class represents an individual expense with two attributes: the amount spent and the expense category.
 public class Expense {
-    
+    public Expense(){
+        //Este será el constructor 
+    }
 }
