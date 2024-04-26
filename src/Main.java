861ce7fa74c9e370472b6c0ac75abd8b89424029
AntonioMonzo4 <amonansu@gmail.com>
2024-04-26 19:23:33 +0200

    Añadir menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 // Main application class that provides a console interface for managing expenses.
 public class Main {
     
+    public static void main(String args[]){
+        System.out.println("Bienvenido al Sistema de Registro de Gastos Personales ");
+
+        System.out.println("\nPor favor, elige una opción: ");
+        System.out.println("1. Agregar un nuevo gasto");
+        System.out.println("2. Listar todos los gastos");
+        System.out.println("3. Sumarizar gastos por categoría");
+        System.out.println("4. Guardar gastos");
+        System.out.println("5. Cargar gastos");
+        System.out.println("6. Salir");
+
+        System.out.println("prueba");
+
+
+    }
+
+    
 }
