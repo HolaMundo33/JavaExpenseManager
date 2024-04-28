519f5f0994b12f0f2b8e267d5affbe1292c38723
AntonioMonzo4 <amonansu@gmail.com>
2024-04-28 12:16:21 +0200

    Creación provisional de la clase EXPENSE

--- src/Expense.java
+++ src/Expense.java
@@ -1,8 +1,15 @@
 // This class represents an individual expense with two attributes: the amount spent and the expense category.
 public class Expense {
-    public Expense(){
-        //Este será el constructor 
-        int gastos;
+
+    int cantidad; //Cantidad de dinero gastado
+    String tipo; //Tipo de gasto: comidad, hogar, factura...
+
+
+    //Constructor de la clase 
+    public Expense(int cantidad, String tipo){
+        this.tipo=tipo;
+        this.cantidad=cantidad;
+        
     }
 
     //Falta crear métodos
