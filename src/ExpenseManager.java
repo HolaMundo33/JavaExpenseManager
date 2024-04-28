5f702ca99e5a4e608c33fe3871e6778d12708718
AlMaMa13 <alexmas_mas@hotmail.com>
2024-04-28 16:25:53 +0200

    Refactorizo la variable (Tipo) por (concepto)

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -6,50 +6,45 @@ import java.util.Scanner;
 
 public class ExpenseManager {
 
-    int cantidad;
-    String concepto;
+	int cantidad;
+	String concepto;
 
-    ArrayList<Expense> listaGastos = new ArrayList<>();
-	
-	
-	public void AgregarGasto () {
-        Scanner scanner = new Scanner(System.in);
+	ArrayList<Expense> listaGastos = new ArrayList<>();
 
+	public void AgregarGasto() {
+		Scanner scanner = new Scanner(System.in);
 
-        Expense gastoAgregado = new Expense(0,"No hay gasto");
-		
-		
+		Expense gastoAgregado = new Expense(0, "No hay gasto");
+
 		System.out.println("Concepto: ");
-		
+
 		gastoAgregado.setConcepto(scanner.nextLine());
-		
+
 		System.out.println("Cantidad: ");
-		
-		gastoAgregado.setCantidad(scanner.nextInt()); 
-		
+
+		gastoAgregado.setCantidad(scanner.nextInt());
 
 		listaGastos.add(gastoAgregado);
 		scanner.close();
 	}
-	
+
 	public void ListarGastos() {
 
-		
-	     for (Expense i : listaGastos) {
-			System.out.println("Monto: "+i.getCantidad()+" Categoría: "+i.getConcepto());
-			
+		for (Expense i : listaGastos) {
+			System.out.println("Monto: " + i.getCantidad() + " Categoría: " + i.getConcepto());
+
 		}
 	}
-	
+
 	public SumarizarGastos() {
 		//comentario de prueba
 		
 	}
-	
+
 	public GuardarGastos() {
 		
 	}
-	
+
 	public CargarGastos() {
 		
 	}
